a2fafa088a1d92eb48fd691b68640f6703d81167
jonesy346 <nilejones2012@gmail.com>
2021-01-27 00:39:18 -0800

    second commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
-const proxy = require('http-proxy-middleware');
+const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-    app.use(proxy('/api/**', { target: 'http://localhost:3000' }));
-    app.use(proxy('/otherApi/**', { target: 'http://localhost:3000' }));
+  app.use(
+    proxy(["/api", "/otherApi"], { target: "http://localhost:5000" })
+  );
 };
